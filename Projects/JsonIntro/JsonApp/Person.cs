a1ee5947277c176d41c10e27d4168ec1c59cee74
Imada <bi1012440@otc.edu>
2026-02-12 10:14:16 -0600

    submission for Module 5: Intro to JSON Guided Lab - Has some fun extras

---
+++ Projects/JsonIntro/JsonApp/Person.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonApp
+{
+    public class Person
+    {
+        // FIELDS
+        public string name { get; set; }
+        public int age { get; set; }
+        public string email { get; set; }
+        public bool isStudent { get; set; }
+
+        // CONSTRUCTOR
+        public Person(string name, int age, string email, bool isStudent)
+        {
+            this.name = name; // use "this" for the declaration NOT the set value
+            this.age = age;
+            this.email = email;
+            this.isStudent = isStudent;
+        }
+
+        // Methods - Extra
+
+        public void SayHi()
+        {
+            Console.WriteLine($"{name} said Hi!");
+        }
+
+        public void Greet(Person person)
+        {
+            Console.WriteLine($"{name}: Hello {person.name}! My name is {name}!");
+        }
+    }
+
+    public static class PersonExtension
+    {
+        public static void PrettyPrintAll(this List<Person> people)
+        {
+            foreach (Person p in people)
+            {
+                Console.WriteLine($"Deserialized Person: \n" +
+                $"\nName: {p.name}" +
+                $"\nAge: {p.age}" +
+                $"\nEmail: {p.email}" +
+                $"\nIs Student: {p.isStudent}"
+                );
+            }
+        }
+
+        public static void SayHiToAll(this List<Person> people) // 'this' makes it so the variable calling the method is used
+        {
+            foreach (Person p in people)
+            {
+                p.SayHi();
+            }
+        }
+
+        public static void GreetAll(this List<Person> people)
+        {
+            // Modulo so for does not exceed and instead greets alternatively
+            for (int i = 0; i < people.Count; i++) // An algorithm with a complexity of O(n^2)
+            {
+                for (int j = 0; j < people.Count - 1; j++)
+                {
+                    people[i].Greet(people[j + 1]);
+                }
+            }
+        }
+    }
+}
---
+++ Projects/JsonIntro/JsonApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace JsonApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // INITIATE
+
+            // declare json lists
+            string serializedPeople;
+            List<Person> deserializedPeople;
+
+            // Create people list
+            List<Person> people = new List<Person>
+            {
+                new("John Doe", 67, "johndoe@email.com", true),
+                new("Jane Smith", 21, "janesmith@email.com", false),
+                new("John Lennon", 60, "johnlennon@email.com", false)
+            };
+
+            // Serialize people and print result
+            serializedPeople = JsonConvert.SerializeObject(people);
+            Console.WriteLine("Serialized JSON: " + serializedPeople);
+
+            // Deserialize people and print individual person
+            deserializedPeople = JsonConvert.DeserializeObject<List<Person>>(serializedPeople);
+            deserializedPeople.PrettyPrintAll();
+
+            //EXTRA LOGIC
+
+            Console.WriteLine("\nExtra non-required logic:\n");
+
+            Console.WriteLine("People saying hello");
+
+            people.SayHiToAll();
+
+            people.GreetAll();
+            
+        }
+    }
+}
