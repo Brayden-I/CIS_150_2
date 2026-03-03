2a3fbaf3e9ba226342ccf8a86e0aafa18f2c237d
Imada <bi1012440@otc.edu>
2026-03-03 09:57:30 -0600

    Latest commit - builds API does not return pokemon

--- Projects/PokeApi/PokeApp/Program.cs
+++ Projects/PokeApi/PokeApp/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace PokeApp
+﻿using PokeApi;
+
+namespace PokeApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static string? input;
+        private static Pokemon pokemon;
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Pokemon Pokedex.");
+
+            while (input != "leave")
+            {
+                // Prompt
+                Console.WriteLine("Please enter the name or id of your requested Pokemon");
+                Console.Write(">> ");
+
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "leave":
+                        continue;
+
+                    default:
+                        input = Console.ReadLine();
+                        pokemon = await Pokedex.Search(input);
+
+                        pokemon.Print();
+                        break;
+                }
+
+            }
         }
     }
 }
--- Projects/PokeApi/PokeApp/pokeapi.cs
+++ Projects/PokeApi/PokeApp/pokeapi.cs
@@ -2,12 +2,66 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace PokeApi
 {
-    public class pokemon
+    public class Pokemon
     {
+        // FIELDS
+        public int id;
+        public string name;
+        public int height;
+        public int weight;
+
+        // Methods
+
+        // print
+        public void Print()
+        {
+            Console.WriteLine($"Pokemon entry for {name}\n-------\nid: {id}\nname: {name}\nheight: {height}\nweight: {weight}\n");
+        }
+    }
+
+    public class Pokedex
+    {
+        // METHODS
+        public static async Task<Pokemon> Search(string name, int id = -1)
+        {
+            try
+            {
+                string url = "https://pokeapi.co/api/v2/pokemon/";
+
+                if (id != -1) // If the user has entered an id, use that input, otherwise use name
+                {
+                    url += $"{id}/";
+                }
+                else
+                {
+                    url += $"{name}/";
+                }
+
+                using HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode) // Check if the api call was successful, if so proceed
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+
+                    return JsonSerializer.Deserialize<Pokemon>(json);
+                }
+
+                return null; // Failed to get pokemon
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+
+                return null; // Unexpected failure to get pokemon
+            }
+        }
 
     }
 }
